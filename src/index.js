a6321865dc2b8201ef2ab438860a8f815005d48e
Bea Jakubcova <jakubea@gmail.com>
2026-09-17 11:55:46 +0200

    Load GTM after app is initialized and broswer is idle

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import { Elm } from './Main.elm';
 import { getFlags } from './js/flags';
 
 import { subscribeGetTranslationsForLanguage } from './js/i18n/';
+
 import { subscribeSetCanonical } from './js/canonical';
+
 import {
   setLangAttribute,
   subscribeSetLanguageStorage,
 } from './js/selectedLanguage';
+
+const loadGoogleTagManager = () => {
+  window.dataLayer = window.dataLayer || [];
+
+  window.dataLayer.push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js',
+  });
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-W682DB8J';
+
+  document.head.appendChild(script);
+};
+
+const loadGoogleTagManagerWhenIdle = () => {
+  if ('requestIdleCallback' in window) {
+    requestIdleCallback(loadGoogleTagManager);
+  } else {
+    setTimeout(loadGoogleTagManager, 2000);
+  }
+};
 
 const initializeElmApp = async () => {
   const flags = await getFlags();
@@ -22,6 +47,7 @@ const initializeElmApp = async () => {
   subscribeSetLanguageStorage(app);
   subscribeGetTranslationsForLanguage(app);
   subscribeSetCanonical(app);
+  loadGoogleTagManagerWhenIdle();
 };
 
 initializeElmApp();
